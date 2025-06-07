cdfa544c2b6c9eb9d1eb90f53102f305fe377f85
franklin534 <franklinl@blacksburgchineseschool.org>
2025-06-06 20:56:55 -0400

    dfdfdfdfdfd

---
+++ public/script.js
@@ -0,0 +1,8 @@
+// This function toggles the visibility of the mobile menu when the hamburger button is clicked
+function toggleMobileMenu() {
+  // Get the element with the id "navLinks"
+  const nav = document.getElementById("navLinks");
+
+  // Toggle the "show" class on the nav element
+  nav.classList.toggle("show");
+}
