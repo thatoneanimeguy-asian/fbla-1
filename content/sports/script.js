709cb2912a97ca51fcb017f92dc61450fa8d91fd
franklin534 <franklinl@blacksburgchineseschool.org>
2025-06-11 20:45:37 -0400

    dfdf

--- content/sports/script.js
+++
@@ -1,5 +0,0 @@
-function speakText(text) {
-  const speech = new SpeechSynthesisUtterance(text);
-  speech.lang = "en-US";  // Change the language if needed
-  window.speechSynthesis.speak(speech);
-}
